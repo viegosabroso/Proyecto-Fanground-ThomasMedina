de1afe4e80ff5dfe424182d0b73dcee4cf6b7678
viegosabroso <thommmmmmas@hotmail.com>
2023-11-02 20:18:55 -0500

    Script: se hizo la data de los conciertos

---
+++ src/components/data/dataconcerts.ts
@@ -0,0 +1,35 @@
+export const dataoncerthistor =[
+    {
+      concertname:"Nigths in the underground",  
+      band: "Entreco",
+      concertimg: "https://i.scdn.co/image/ab6761610000e5ebc54e9851ddfb5d73f4a46cb9",
+      date: "2023-11-05"
+
+    },
+    {
+      concertname:"Party in latino power",  
+      band: "Kidchen",
+      concertimg: "https://i.scdn.co/image/ab6761610000e5eba1496f1ee881fe416df8388b",
+      date: "2023-11-05"
+    },
+    {
+      concertname:"Goodbye from LHMF",  
+      band: "LHMF",
+      concertimg: "https://i.scdn.co/image/ab6761610000e5ebe6192320d3d59eb34dcfc10d",
+      date: "2023-11-05"
+    },
+    {
+      concertname:"Punk and plants",  
+      band: "VC",
+      concertimg: "https://f4.bcbits.com/img/a2093551838_65",
+      date: "2023-11-05"
+
+    },
+    {
+      concertname:"Trash in and out",  
+      band: "basurastillada",
+      concertimg: "https://f4.bcbits.com/img/0026616715_10.jpg",
+      date: "2023-11-05"
+    }
+  ];
+  
